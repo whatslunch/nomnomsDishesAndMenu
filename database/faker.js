41b3b77a7f2ac0692f91dcef0c29940d0c8b6bb4
Mary Matthews <matthewsmp@gmail.com>
2018-10-25 17:31:11 -0700

    set up urls for photos in s3

--- database/faker.js
+++ database/faker.js
@@ -1,6 +1,7 @@
 var faker = require('faker');
 var fs = require('fs');
 
+// SEED restaurant table
 var randomRestName;
 var randomDishName;
 var randomDishDescription;
@@ -15,6 +16,7 @@ while (x < 100) {
   x++;
 }
 
+//  SEED dishes table
 var randomNum;
 var randomDescription;
 var precision = 100;
@@ -34,3 +36,18 @@ while (y < 100) {
   }
   y++;
 }
+
+// SEED photos table
+var filePathBase = 'https://s3.us-east-2.amazonaws.com/yelpsfphotos/';
+var photoURLs = ['download (1).jpeg', 'download.jpeg', 'images (1).jpeg', 'images (10).jpeg', 'images (11).jpeg', 'images (12).jpeg', 'images (13).jpeg', 'images (14).jpeg', 'images (15).jpeg', 'images (16).jpeg', 'images (17).jpeg', 'images (18).jpeg', 'images (19).jpeg', 'images (2).jpeg', 'images (20).jpeg', 'images (21).jpeg', 'images (22).jpeg', 'images (23).jpeg', 'images (24).jpeg', 'images (25).jpeg', 'images (26).jpeg', 'images (27).jpeg', 'images (28).jpeg', 'images (29).jpeg', 'images (3).jpeg', 'images (30).jpeg', 'images (31).jpeg', 'images (5).jpeg', 'images (6).jpeg', 'images (7).jpeg', 'images (8).jpeg', 'images (9).jpeg', 'images.jpeg', 'ls.jpg'];
+
+var urlsWithBase = photoURLs.map(url => {
+  return filePathBase + url;
+});
+
+// photoURLS length is 34... loop through these a few times to insert rows for photos after dinner!
+
+
+
+
+
